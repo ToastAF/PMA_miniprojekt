9c68a6b52e06aa8d29f08a3982794f6224135b49
SoupFanAccount <112694021+SoupFanAccount@users.noreply.github.com>
2023-04-18 14:08:11 +0200

    Lejede med knapper

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Scanner;
 import javax.swing.*;
 import java.awt.*;
@@ -28,7 +30,14 @@ public class Main {
         frame.getContentPane().add(BorderLayout.EAST, picturePanel);
 
         JPanel buttonPanel = new JPanel();
-        JButton firstButton = new JButton("Test Button");
+        JButton firstButton = new JButton("FROGIFY!", new ImageIcon("Characters/Ohnoman-littol.png"));
+        firstButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Stinky haha poop");
+            }
+        });
+        firstButton.setVisible(false);
         JButton secondButton = new JButton("Test Button 2");
         frame.getContentPane().add(BorderLayout.SOUTH, buttonPanel);
         buttonPanel.add(firstButton);
